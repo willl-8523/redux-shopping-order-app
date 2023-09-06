02ebf115652dfd21bb9fc0ec4743393de06cc2c7
willl85 <ngowilfried@gmail.com>
2023-09-06 19:51:34 +0200

    Add CartItem Component dynamically

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,26 @@
+import { useSelector } from 'react-redux';
 import Card from '../UI/Card';
 import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cart.items);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {cartItems.map((mapItem) => (
+          <CartItem
+            key={mapItem.id}
+            item={{
+              title: mapItem.name,
+              quantity: mapItem.quantity,
+              total: mapItem.totalPrice,
+              price: mapItem.price,
+            }}
+          />
+        ))}
       </ul>
     </Card>
   );
