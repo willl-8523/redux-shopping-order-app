75efb7109db36c1bd1749f254a1fde889ae9d4e8
willl85 <ngowilfried@gmail.com>
2023-09-07 13:51:51 +0200

    Add fetch in to create or update cart

--- src/App.js
+++ src/App.js
@@ -2,9 +2,25 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch(
+      'https://react-http-d6045-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+      {
+        /**
+         * La differnece entre POST et PUT est que PUT MAJ s'il existe et POST cree
+         * meme s'il existe
+         */
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]);
   
   return (
     <Layout>
